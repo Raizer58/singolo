37d453c9370e5c99ffd67672e9e1e9686c935738
Raizer58 <Raizer9058w@gmail.com>
2020-03-13 20:29:40 +0300

    refactor: change  menu switch implementation

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 window.addEventListener('scroll', menuSwitching);
 
 function menuSwitching(){
-    let currentPosition = window.scrollY;
 
     document.querySelector('#menu-block-link').querySelectorAll('a').forEach(el=> el.classList.remove('active-menu-link'));
 
-    if (currentPosition >= document.querySelector('#home-mark').getBoundingClientRect().top + currentPosition && 
-        currentPosition <= document.querySelector('#our-services-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight){
+    if (document.querySelector('#home-mark').getBoundingClientRect().top <= 0 && 
+        document.querySelector('#our-services-mark').getBoundingClientRect().top - 10>= 0){
         document.querySelector('#link-home-mark').classList.add('active-menu-link')
     };
 
-    if (currentPosition >= document.querySelector('#our-services-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight &&
-        currentPosition <= document.querySelector('#portfolio-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight){
+    if (document.querySelector('#our-services-mark').getBoundingClientRect().top - 10 <= 0 &&
+        document.querySelector('#portfolio-mark').getBoundingClientRect().top - 10 >= 0){
         document.querySelector('#link-our-services-mark').classList.add('active-menu-link')
     };
 
-    if (currentPosition >= document.querySelector('#portfolio-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight &&
-        currentPosition <= document.querySelector('#about-as-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight){
+    if (document.querySelector('#portfolio-mark').getBoundingClientRect().top - 10<= 0 &&
+        document.querySelector('#about-as-mark').getBoundingClientRect().top - 10 >= 0){
         document.querySelector('#link-portfolio-mark').classList.add('active-menu-link')
     };
 
-    if (currentPosition >= document.querySelector('#about-as-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight &&
-        currentPosition <= document.querySelector('#get-a-quote-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight){
+    if (document.querySelector('#about-as-mark').getBoundingClientRect().top - 10 <= 0 &&
+        document.querySelector('#get-a-quote-mark').getBoundingClientRect().top - 10 >= 0){
         document.querySelector('#link-about-as-mark').classList.add('active-menu-link')
     };
 
-    if (currentPosition >= document.querySelector('#get-a-quote-mark').getBoundingClientRect().top + currentPosition - document.querySelector('header').offsetHeight){
+    if (document.querySelector('#get-a-quote-mark').getBoundingClientRect().top - 10 <= 0){
         document.querySelector('#link-get-a-quote-mark').classList.add('active-menu-link')
     };
 
